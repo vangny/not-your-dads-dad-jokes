95c5e4693af7ca1d920c1f36abdd1113829f2750
vangny <vang.nathany@gmail.com>
2019-04-03 23:23:39 -0500

    add basic button to send api request to generate a joke

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,15 +1,22 @@
 import React, { Component } from 'react';
 import { render } from 'react-dom';
+import axios from 'axios';
 
 class App extends Component {
   constructor(props) {
     super(props);
+    this.getNewJoke = this.getNewJoke.bind(this);
+  }
+
+  getNewJoke() {
+    axios.get('/api/dad-jokes');
+    // console.log('hi');
   }
 
   render() {
     return (
       <div>
-        hello world
+        <button onClick={this.getNewJoke}>Hi World</button>
       </div>
     );
   }
