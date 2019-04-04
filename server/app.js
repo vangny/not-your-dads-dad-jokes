1c84e31f218738b572761421ed7bbe823c22d885
vangny <vang.nathany@gmail.com>
2019-04-03 20:37:25 -0500

    fix typo on line 4

--- server/app.js
+++ server/app.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const bodyparser = require('body-parser');
 
-const app = expess();
+const app = express();
 
 app.use(express.static(`${__dirname}/../client/dist`));
 app.use(bodyparser.json());
