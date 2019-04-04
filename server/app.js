140a49d3bcf3891c2650c30b2d48371b16737176
vangny <vang.nathany@gmail.com>
2019-04-03 23:21:37 -0500

    add api request that generates jokes

--- server/app.js
+++ server/app.js
@@ -1,11 +1,15 @@
 const express = require('express');
 const bodyparser = require('body-parser');
-const dadJokes = require('./dadJokeGenerator');
+const dadJokes = require('./jokeGenerator');
 
 const app = express();
 
 app.use(express.static(`${__dirname}/../client/dist`));
 app.use(bodyparser.json());
 app.use(bodyparser.urlencoded({extended: false}));
+
+app.get('/api/dad-jokes', (req, res) => {
+  dadJokes();
+})
 
 module.exports = app;
