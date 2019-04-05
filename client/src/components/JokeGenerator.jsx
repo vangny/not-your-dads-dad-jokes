69addbc53231f88085e551c0d9e88451b8bf7090
vangny <vang.nathany@gmail.com>
2019-04-04 21:57:17 -0500

    move getNewJoke functionality from App to JokeGenerator component

--- client/src/components/JokeGenerator.jsx
+++ client/src/components/JokeGenerator.jsx
@@ -3,12 +3,47 @@ import React, { Component } from 'react';
 class JokeGenerator extends Component {
   constructor(props) {
     super(props);
+    this.state = {
+      joke: null,
+    }
+    
+    this.getNewJoke = this.getNewJoke.bind(this);
+  }
+
+  getNewJoke() {
+    axios.get('/api/dad-jokes')
+      .then((response) => {
+        this.setState({
+          joke: response.data,
+        });
+      });
+  }
+
+  renderJoke() {
+    const { joke } = this.state;
+
+    if (!!joke) {
+      return(
+        <p>{joke}</p>
+      );
+    } else {
+      return (
+        <p>Go ahead and generate a dad joke!</p>
+      );
+    }
   }
 
   render() {
     return (
       <div>
-
+        <div className='joke-container'>
+          <div className='text-container'>
+            {this.renderJoke()}
+          </div>
+          <div className='button-container'>
+            <button className='button-generate-joke' onClick={this.getNewJoke}>Generate Joke</button>
+          </div>
+        </div>
       </div>
     );
   }
