d3e0ac502b18824ac4d770d2eb489b231d26cc4d
vangny <vang.nathany@gmail.com>
2019-04-04 23:58:20 -0500

    implement image logo into Navbar component

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -10,17 +10,21 @@ class Navbar extends Component {
   render() {
     return (
       <div className='navbar-container'>
-        <Link className='nav-logo-container' to='/'>
-          <div>
-            <span>NYD Dad Jokes</span>
+        <Link className='nav-logo-container nav-item' to='/'>
+          <div className='nav-logo'>
+            <img className='logo-img' src={require('../../../images/nyddj-logo-38x50.png')}/>
+            <span>Not Your Dad's Dad Jokes</span>
+          </div>
+          <div className='nav-logo-text'>
+            
           </div>
         </Link>
-        <Link className='nav-home-container' to='/'>
+        <Link className='nav-home-container nav-item' to='/'>
           <div>
             <span>Home</span>
           </div>
         </Link>
-        <Link className='nav-history-container' to='/history'>
+        <Link className='nav-history-container nav-item' to='/history'>
           <div>
             <span>History</span>
           </div> 
