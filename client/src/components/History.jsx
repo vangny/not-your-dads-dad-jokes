8bdc93af321c8a1b1105fdae8d7444de716d7570
vangny <vang.nathany@gmail.com>
2019-04-04 20:49:57 -0500

    add basic react component for history component

---
+++ client/src/components/History.jsx
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+
+class History extends Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <div>
+
+      </div>
+    );
+  }
+}
+
+export default History;
