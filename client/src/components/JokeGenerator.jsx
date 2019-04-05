6bc38c3fd1ede9ee03a1e357e62a43accbe54ad1
vangny <vang.nathany@gmail.com>
2019-04-04 20:39:58 -0500

    add basic react component for Joke Generator component

---
+++ client/src/components/JokeGenerator.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class JokeGenerator extends Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <div></div>
+    );
+  }
+}
