3294d54f2c22a89b3d6fedcdc7688d7060783d84
vangny <vang.nathany@gmail.com>
2019-04-05 14:13:39 -0500

    add conditional that makes rendering depend on existing cache

--- client/src/components/History.jsx
+++ client/src/components/History.jsx
@@ -1,15 +1,38 @@
 import React, { Component } from 'react';
+import HistoryJoke from './HistoryJoke';
+require('../styles/history.css')
 
 class History extends Component {
   constructor(props) {
     super(props);
   }
 
+  renderJokes() {
+    let jokes = JSON.parse(sessionStorage.getItem('jokes'));
+
+    if (!!jokes) {
+      return (
+        <div className='history-container'>
+          {jokes.map((joke) => <HistoryJoke joke={joke} />)}
+        </div>
+        
+      );
+    } else {
+      return (
+        <div className='history-container'>
+          <div className='empty-container'>
+            <span className='empty-paragraph'>
+              It looks a little empty here 😐
+            </span>
+          </div>
+        </div>
+      );
+    }
+  }
+
   render() {
     return (
-      <div>
-
-      </div>
+      this.renderJokes()
     );
   }
 }
